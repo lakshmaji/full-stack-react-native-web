0e2b0d563650f7b4bffb1115abebca8d43b84dcd
Lakshmaji <lakshmajee88@gmail.com>
2020-04-08 19:57:00 +0530

    WIP

--- packages/commons/src/views/Login/Login.js
+++ packages/commons/src/views/Login/Login.js
@@ -1,7 +1,14 @@
 import React, { useState } from "react";
 import { View, Text, Image } from "react-native";
 import { Button, Searchbar, withTheme } from "react-native-paper";
-import Logo from "../../../imgs/address.svg";
+// import Logo from "../../../imgs/address.svg";
+// import Logo from "../../../imgs/bbc.svg";
+import Logo from "../../../imgs/bbc-data.svg";
+
+// import Logo, { ReactComponent as Logo } from "../../../imgs/address.svg";
+// import {ReactComponent as Logo} from "../../../imgs/bbc.svg";
+// import { SvgXml } from 'react-native-svg';
+
 import { styles } from "./Login.styles";
 
 const Login = () => {
@@ -10,14 +17,15 @@ const Login = () => {
       <View style={styles.container}>
         <View style={styles.content}>
           <Logo
-            width={100}
-            height={100}
+            // width={100}
+            // height={100}
             style={{
-              backgroundColor: "green",
+              backgroundColor: "yellow",
               color: "white",
               alignSelf: "center"
             }}
           />
+          {/* <SvgXml width="200" height="200" xml={Logo} /> */}
           <View>
             <Text style={styles.inputLabel}>Please login to continue</Text>
           </View>
