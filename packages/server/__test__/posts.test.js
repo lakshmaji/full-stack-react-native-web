227f202f55d89defddfb2b03fa03f075e11c731f
Lakshmaji <lakshmajee88@gmail.com>
2020-04-10 22:14:52 +0530

    API test is working without done cb reference

--- packages/server/__test__/posts.test.js
+++ packages/server/__test__/posts.test.js
@@ -3,11 +3,10 @@ const server = require('../app');
 
 
 describe("Testing the posts API", () => {
-    it("tests the base route and returns true for status", async done => {
+    it("tests the base route and returns true for status", async () => {
         const response = await supertest(server).get('/');
         expect(response.status).toBe(200)
         expect(response.body.message).toBe('API')
-        done()
     });
 
 })
