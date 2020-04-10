a028b94d2279651df895081120db7d317fedb97a
Lakshmaji <lakshmajee88@gmail.com>
2020-04-10 23:55:21 +0530

    Create post 401 testcase

--- packages/server/__test__/posts.test.js
+++ packages/server/__test__/posts.test.js
@@ -9,4 +9,20 @@ describe("Testing the posts API", () => {
         expect(response.body.message).toBe('API')
     });
 
+    it("Should not allow to create a post, as auth token was not passed", async () => {
+        const response = await supertest(server).post('/api/posts').send({
+            title: 'Bhagavad gita',
+            postContent: 'The Bhagavad Gita, often referred to as the Gita, is a 700-verse Hindu scripture that is part of the epic Mahabharata. ',
+            createdBy: 'Some mock user'
+        });
+        expect(response.status).toBe(401);
+        expect(response.body.message).toBe('No token provided.');
+    })
+
+    it("Should be able to get posts ", async () => {
+        const response = await supertest(server).get('/api/posts');
+        expect(response.status).toBe(200);
+        expect(response.body);
+    })
+
 })
