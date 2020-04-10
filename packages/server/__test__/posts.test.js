066a8970ca86f4f8d699a427a40d6d9fbce3412b
Lakshmaji <lakshmajee88@gmail.com>
2020-04-11 01:25:20 +0530

    Basic test cases with posts test suite

--- packages/server/__test__/posts.test.js
+++ packages/server/__test__/posts.test.js
@@ -1,28 +1,75 @@
 const supertest = require('supertest');
 const server = require('../app');
 
+let token;
 
-describe("Testing the posts API", () => {
-    it("tests the base route and returns true for status", async () => {
-        const response = await supertest(server).get('/');
-        expect(response.status).toBe(200)
-        expect(response.body.message).toBe('API')
-    });
+describe("load app instance and get token", () => {
 
-    it("Should not allow to create a post, as auth token was not passed", async () => {
-        const response = await supertest(server).post('/api/posts').send({
-            title: 'Bhagavad gita',
-            postContent: 'The Bhagavad Gita, often referred to as the Gita, is a 700-verse Hindu scripture that is part of the epic Mahabharata. ',
-            createdBy: 'Some mock user'
-        });
-        expect(response.status).toBe(401);
-        expect(response.body.message).toBe('No token provided.');
+    beforeAll(async () => {
+        const response = await supertest(server)
+            .post('/api/auth/register')
+            .send({
+                email: 'lakshmaji@gmail.com',
+                password: 'Master@123',
+                name: 'Lakshmaji'
+            });
+        if (response.body) {
+            token = response.body.token;
+        }
     })
 
-    it("Should be able to get posts ", async () => {
-        const response = await supertest(server).get('/api/posts');
-        expect(response.status).toBe(200);
-        expect(response.body);
+    afterAll(async () => {
+        if (server.close) {
+            await server.close()
+        }
+    })
+
+    describe("App base api check", () => {
+        it("tests the base route and returns true for status", async () => {
+            const response = await supertest(server).get('/');
+            expect(response.status).toBe(200)
+            expect(response.body.message).toBe('API')
+        });
+    })
+
+
+    describe("Testing the posts API", () => {
+        it("Should not allow to create a post, as auth token was not passed", async () => {
+            const response = await supertest(server).post('/api/posts').send({
+                title: 'Bhagavad gita',
+                postContent: 'The Bhagavad Gita, often referred to as the Gita, is a 700-verse Hindu scripture that is part of the epic Mahabharata. ',
+                createdBy: 'Some mock user'
+            });
+            expect(response.status).toBe(401);
+            expect(response.body.message).toBe('No token provided.');
+        })
+
+        // it("Should return empty posts ", async () => {
+        //     const response = await supertest(server).get('/api/posts');
+        //     expect(response.status).toBe(200);
+        //     expect(response.body);
+        //     expect(response.body.length).toBe(0);
+        // })
+
+        it("Should be able to create a post", async () => {
+            const response = await supertest(server).post('/api/posts')
+                .set('Authorization', `Bearer ${token}`)
+                .send({
+                    title: 'Bhagavad gita',
+                    postContent: 'The Bhagavad Gita, often referred to as the Gita, is a 700-verse Hindu scripture that is part of the epic Mahabharata. ',
+                    createdBy: 'Some mock user'
+                });
+            expect(response.status).toBe(200);
+            expect(response.type).toBe('application/json');
+        })
+
+        it("Should be able to get posts ", async () => {
+            const response = await supertest(server).get('/api/posts');
+            expect(response.status).toBe(200);
+            expect(response.body);
+            // console.log('--------', response.body, token)
+        })
+
     })
 
 })
