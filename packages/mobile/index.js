416dea00b1e192a420741b34a9131c9ff604908a
Lakshmaji <lakshmajee88@gmail.com>
2020-04-12 19:37:58 +0530

    Corrected App import for mobile

--- packages/mobile/index.js
+++ packages/mobile/index.js
@@ -2,8 +2,8 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
-import App from './App.mobile';
-import {name as appName} from './app.json';
+import { AppRegistry } from 'react-native';
+import App from './App';
+import { name as appName } from './app.json';
 import React from "react";
 AppRegistry.registerComponent(appName, () => App);
